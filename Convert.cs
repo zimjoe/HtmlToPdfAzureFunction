4d7e56fabf8a5d7ceb6ba8284c8123ffde6ef0f5
zimjoe <zimjoe@gmail.com>
2023-01-22 21:29:36 -0500

    Initial commit of local working process.  We will see if the Action can do the rest

---
+++ Convert.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using System.Net.Http;
+
+namespace HtmlToPdfFunction
+{
+    public  class Convert
+    {
+        private readonly HttpClient _client;
+        private readonly IConverter _converter;
+
+
+        public Convert(IHttpClientFactory httpClientFactory, IConverter converter)
+        {
+            this._client = httpClientFactory.CreateClient();
+            this._converter = converter;
+        }
+        [FunctionName("Convert")]
+        public  async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                //grab the incoming file
+                var file = req.Form.Files["file"];
+               // read it
+                using var reader = new StreamReader(file.OpenReadStream());
+
+                var fileString =    await reader.ReadToEndAsync();
+
+                var doc = new HtmlToPdfDocument()
+                {
+                    GlobalSettings = {
+                        ColorMode = ColorMode.Color,
+                        Orientation = Orientation.Landscape,
+                        PaperSize = PaperKind.A4Plus,
+                    },
+                                    Objects = {
+                        new ObjectSettings() {
+                            PagesCount = true,
+                            HtmlContent = fileString,
+                            WebSettings = { DefaultEncoding = "utf-8" },
+                            //HeaderSettings = { FontSize = 9, Right = "Page [page] of [toPage]", Line = true, Spacing = 2.812 }
+                        }
+                    }
+                };
+
+                byte[] pdf = _converter.Convert(doc);
+
+                log.LogInformation("Successfull PDF");
+
+                return new FileContentResult(pdf, "application/pdf")
+                {
+                    FileDownloadName = "test.pdf"
+                };
+            }
+            catch (Exception ex)
+            {
+                log.LogError(new EventId(),ex, "Error Creating PDF");
+                return new BadRequestObjectResult(ex);
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+
+[assembly: FunctionsStartup(typeof(HtmlToPdfFunction.Startup))]
+
+namespace HtmlToPdfFunction
+{
+
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+            // Add converter to DI
+            builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+           
+        }
+    }
+}
